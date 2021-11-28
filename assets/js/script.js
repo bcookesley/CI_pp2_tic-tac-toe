a189052db7e9e37debb1b506023737e661a376ab
bcookesley <ben.cookesley@gmail.com>
2021-11-28 11:26:44 +0000

    add scores section and game overview

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const tiles = Array.from(document.querySelectorAll('.tile'));
+    const boxes = Array.from(document.querySelectorAll('.box'));
     const playerDisplay = document.querySelector('.display-player');
     const resetButton = document.querySelector('#reset');
     const announcer = document.querySelector('.announcer');
@@ -64,8 +64,8 @@ window.addEventListener('DOMContentLoaded', () => {
         announcer.classList.remove('hide');
     };
 
-    const isValidAction = (tile) => {
-        if (tile.innerText === 'X' || tile.innerText === 'O') {
+    const isValidAction = (box) => {
+        if (box.innerText === 'X' || box.innerText === 'O') {
             return false;
         }
 
@@ -83,10 +83,10 @@ window.addEventListener('DOMContentLoaded', () => {
         playerDisplay.classList.add(`player${currentPlayer}`);
     }
 
-    const userAction = (tile, index) => {
-        if (isValidAction(tile) && isGameActive) {
-            tile.innerText = currentPlayer;
-            tile.classList.add(`player${currentPlayer}`);
+    const userAction = (box, index) => {
+        if (isValidAction(box) && isGameActive) {
+            box.innerText = currentPlayer;
+            box.classList.add(`player${currentPlayer}`);
             updateBoard(index);
             handleResultValidation();
             changePlayer();
@@ -102,15 +102,15 @@ window.addEventListener('DOMContentLoaded', () => {
             changePlayer();
         }
 
-        tiles.forEach(tile => {
-            tile.innerText = '';
-            tile.classList.remove('playerX');
-            tile.classList.remove('playerO');
+        boxes.forEach(box => {
+            box.innerText = '';
+            box.classList.remove('playerX');
+            box.classList.remove('playerO');
         });
     }
 
-    tiles.forEach( (tile, index) => {
-        tile.addEventListener('click', () => userAction(tile, index));
+    boxes.forEach( (box, index) => {
+        box.addEventListener('click', () => userAction(box, index));
     });
 
     resetButton.addEventListener('click', resetBoard);
