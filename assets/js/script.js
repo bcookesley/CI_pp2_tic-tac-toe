6e8374d826ddebc666b15b36c4f851b6d855412e
bcookesley <ben.cookesley@gmail.com>
2021-12-13 14:43:06 +0000

    delete commented code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,3 @@
-/* Almost all of this code was copied from an external source on Youtube. All credits and links to this video are in the README.md file.
-While a simple game - my JavaScript progression and understanding were not good enough to make a game such as this. Due to the amount of 
-time I had to complete this unit and hand my project in I had to find how others had written and coded this game (there are many!).
-
-I typed this all out myself and I have watched, written and rewritten lots of this code. I have also added some of my own, but this was also largely inspired and taken from 
-the Love Maths walkthrough project.
-
-*/
-
 // Waits for DOM content to load
 
 document.addEventListener('DOMContentLoaded', () => {
