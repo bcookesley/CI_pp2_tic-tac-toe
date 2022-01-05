d8fe1c92c2c603a812bbd8382609b4e7bb8f7372
bcookesley <ben.cookesley@gmail.com>
2022-01-05 10:09:35 +0000

    add winning combinations to JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,9 +17,20 @@ document.addEventListener('DOMContentLoaded', () => {
     let winnerPage = document.getElementById("winner-page");
     let winningName = document.getElementById("winner-name");
     let reset = document.getElementById("quit");
+    isPlayerMove = true;
 
-    const ticTacToeGame = new TicTacToeGame();
-    //ticTacToeGame.start();
+    let winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+
 
 
 
