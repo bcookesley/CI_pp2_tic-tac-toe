43bc5fc76f4a82e389d08afbd618a8a000254268
bcookesley <ben.cookesley@gmail.com>
2022-01-09 12:42:12 +0000

    add final code for contact form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,15 +1,22 @@
  window.onload = function() {
             document.getElementById('contact-form').addEventListener('submit', function(event) {
                 event.preventDefault();
-                // generate a five digit number for the contact_number variable
-                this.contact_number.value = Math.random() * 100000 | 0;
                 // these IDs from the previous steps
                 emailjs.init('user_e9YcwY3enVlaEkAqKTrG4');
-                emailjs.sendForm('contact_service', 'contact_form', this)
+                emailjs.sendForm('service_ahguzk1', 'contact_form', this)
                     .then(function() {
                         console.log('SUCCESS!');
                     }, function(error) {
                         console.log('FAILED...', error);
                     });
+                    
             });
         }
+
+        function messageSent() {
+            let html = `
+                    `
+            document.getElementById('container').innerHTML = html;
+        }
+
+        
