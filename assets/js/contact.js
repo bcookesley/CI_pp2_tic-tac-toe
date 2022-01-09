8a81795a9ae930e726bfe34966d60fa5ee0fd121
bcookesley <ben.cookesley@gmail.com>
2022-01-09 13:12:53 +0000

    add thank you message for contact form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,22 +1,26 @@
- window.onload = function() {
-            document.getElementById('contact-form').addEventListener('submit', function(event) {
-                event.preventDefault();
-                // these IDs from the previous steps
-                emailjs.init('user_e9YcwY3enVlaEkAqKTrG4');
-                emailjs.sendForm('service_ahguzk1', 'contact_form', this)
-                    .then(function() {
-                        console.log('SUCCESS!');
-                    }, function(error) {
-                        console.log('FAILED...', error);
-                    });
-                    
-            });
-        }
+ window.onload = function () {
+     document.getElementById('contact-form').addEventListener('submit', function (event) {
+         event.preventDefault();
+         // these IDs from the previous steps
+         emailjs.init('user_e9YcwY3enVlaEkAqKTrG4');
+         emailjs.sendForm('service_ahguzk1', 'contact_form', this)
+             .then(function () {
+                 console.log('SUCCESS!');
+             }, function (error) {
+                 console.log('FAILED...', error);
+             });
+             messageSent();
+     });
+ }
 
-        function messageSent() {
-            let html = `
-                    `
-            document.getElementById('container').innerHTML = html;
-        }
-
-        
+ function messageSent() {
+     let html = `
+                <div id="container">
+                <h2>Thanks for your message!</h2>
+                </div>
+                <div id="back-btn">
+                <a href="./index.html">Back to Game</a>
+                </div>
+                `
+     document.getElementById('container').innerHTML = html;
+ }
